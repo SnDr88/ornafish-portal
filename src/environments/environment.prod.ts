62d9cc7f21cb9934c4be9e0ac9603573af5f30f2
Sander Heijnen <sanderheijnen@gmail.com>
2025-04-10 09:12:27 +0200

    environment change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'api-ornafish-bmazhpbvf4bgh0du.westeurope-01.azurewebsites.net', // ✅ live API op Azure
+    apiUrl: 'https://api-ornafish-bmazhpbvf4bgh0du.westeurope-01.azurewebsites.net', // ✅ live API op Azure
   };
