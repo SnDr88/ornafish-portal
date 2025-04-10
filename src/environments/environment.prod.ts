ccd9dfac6be1b0a85977ebb4ed10f3aef19e905a
Sander Heijnen <sanderheijnen@gmail.com>
2025-04-10 08:42:59 +0200

    test deployment

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    apiUrl: 'api-ornafish-bmazhpbvf4bgh0du.westeurope-01.azurewebsites.net', // ✅ live API op Azure
+  };
---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    apiUrl: 'https://api-dev-ornafish-def8dqatgjgwcpbf.westeurope-01.azurewebsites.net', // 👈 staging gebruikt dev-API
+};
